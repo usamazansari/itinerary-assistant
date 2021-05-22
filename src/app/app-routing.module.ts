9ac6144855023b49a3c2b1d8f120507136b5050c
Usama Ansari <45492419+usama251993@users.noreply.github.com>
2021-05-22 15:43:41 +0530

    :calendar: 2021-05-22 :truck: usama251993-develop
    > Migrated to Angular 12
    
    - CHANGES
      1. Moved the entire project from `client/` to `./`
      2. Updated Angular to `12.0.x`
    
    - PENDING
      1. Remove `NgRx` library for now
      2. Create Models to make the application functional
      3. Fetch Trip List from Firebase
      4. Deploy on Firebase

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { environment } from '@environments/environment';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('@core/core.module').then((_ => _.AppCoreModule))
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: !environment.production
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
