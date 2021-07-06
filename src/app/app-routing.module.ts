532c18feeac6b3f0dbe1b31def27519c13902c30
Usama Ansari <45492419+usama251993@users.noreply.github.com>
2021-07-06 09:43:38 +0530

    ♻️ Modularization Normalization #31
    🔨 Fixed Routing to the Trip Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     path: APP_ROUTES.EMPTY,
     loadChildren: () => import('./features/home/home.module').then(module => module.HomeModule)
   },
+  {
+    path: APP_ROUTES.TRIP,
+    loadChildren: () => import('./features/trip/trip.module').then(module => module.TripModule)
+  }
 ];
 
 @NgModule({
