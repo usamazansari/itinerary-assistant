b386121ecd7c91a060ba09a028c6b46e2e284e06
Usama Ansari <45492419+usama251993@users.noreply.github.com>
2021-07-28 17:45:14 +0530

    🎨 #88 FontAwesome Setup Complete

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     '<rootDir>/libs/ui/core',
     '<rootDir>/libs/ui/home',
     '<rootDir>/libs/ui/util/material',
-    '<rootDir>/libs/ui/util/tailwind'
+    '<rootDir>/libs/ui/util/tailwind',
+    '<rootDir>/libs/ui/util/icon'
   ]
 };
