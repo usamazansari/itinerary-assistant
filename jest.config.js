f9587402a9aa4530093e49a278b1cad4c92c1dac
Usama Ansari <45492419+usama251993@users.noreply.github.com>
2021-07-30 15:25:12 +0530

    📦 #93 Removed UI-Home Library

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
     '<rootDir>/apps/api',
     '<rootDir>/apps/ui',
     '<rootDir>/libs/ui/core',
-    '<rootDir>/libs/ui/home',
     '<rootDir>/libs/ui/util/material',
     '<rootDir>/libs/ui/util/tailwind',
     '<rootDir>/libs/ui/util/icon',
