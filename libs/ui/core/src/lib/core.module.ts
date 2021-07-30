0264ddcfbae91a0fd7cc16d0f49d6ff43e785404
Usama Ansari <45492419+usama251993@users.noreply.github.com>
2021-07-30 14:16:41 +0530

    📦 #89 Removed Core Module Deprecations

--- libs/ui/core/src/lib/core.module.ts
+++ libs/ui/core/src/lib/core.module.ts
@@ -1,28 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MaterialModule } from '@itinerary-assistant/ui/material';
-import { IconModule } from '@itinerary-assistant/ui/icon';
+const declarations: never[] = [];
 
-import { FooterComponent } from './components/footer/footer-container/footer/footer.component';
-import { FooterContainerComponent } from './components/footer/footer-container/footer-container.component';
-
-import { NavbarComponent } from './components/navigation/navbar/navbar-container/navbar/navbar.component';
-import { NavbarContainerComponent } from './components/navigation/navbar/navbar-container/navbar-container.component';
-
-import { SidenavComponent } from './components/navigation/sidenav/sidenav-container/sidenav/sidenav.component';
-import { SidenavContainerComponent } from './components/navigation/sidenav/sidenav-container/sidenav-container.component';
-
-const declarations = [
-  FooterComponent,
-  FooterContainerComponent,
-  NavbarComponent,
-  NavbarContainerComponent,
-  SidenavComponent,
-  SidenavContainerComponent
-];
-
-const imports = [CommonModule, MaterialModule, IconModule];
+const imports = [CommonModule];
 const exports = [...imports, ...declarations];
 
 @NgModule({ imports, declarations, exports })
