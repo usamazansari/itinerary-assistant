4146dc9a9cec158453cae8187f2916a5ce7adb4c
Usama Ansari <45492419+usama251993@users.noreply.github.com>
2021-07-31 18:52:10 +0530

    👽 #78 Lumberjack setup complete

--- libs/ui/core/src/lib/core.module.ts
+++ libs/ui/core/src/lib/core.module.ts
@@ -1,10 +1,17 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LumberjackModule } from '@ngworker/lumberjack';
+import { LumberjackConsoleDriverModule } from '@ngworker/lumberjack/console-driver';
+
 const declarations: never[] = [];
 
-const imports = [CommonModule];
-const exports = [...imports, ...declarations];
+const imports = [
+  CommonModule,
+  LumberjackModule.forRoot(),
+  LumberjackConsoleDriverModule.forRoot()
+];
+const exports = [LumberjackModule, LumberjackConsoleDriverModule];
 
 @NgModule({ imports, declarations, exports })
 export class CoreModule {
