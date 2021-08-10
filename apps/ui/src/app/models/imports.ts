f84a4adb29449ca74c2a0ade77f295e5f498a84b
Usama Ansari <usama251993@gmail.com>
2021-08-10 10:46:25 +0530

    👊 #75 Optimized imports in apps/ui

--- apps/ui/src/app/models/imports.ts
+++
@@ -1,5 +0,0 @@
-export type { LayoutDataModel } from '@itinerary-assistant/ui/layout';
-export { LayoutDataStub } from '@itinerary-assistant/ui/layout';
-
-export type { IconModel } from '@itinerary-assistant/ui/core';
-export { IconStub } from '@itinerary-assistant/ui/core';
