c99251bfa4ede29d2386189a34c5bfd48289afa9
Usama Ansari <usama251993@gmail.com>
2021-08-14 11:18:01 +0530

    🚚 #109 Cleanup after Library Initialization

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,13 @@ module.exports = {
   projects: [
     '<rootDir>/apps/api',
     '<rootDir>/apps/ui',
+    '<rootDir>/libs/api/core',
+    '<rootDir>/libs/shared',
     '<rootDir>/libs/ui/core',
     '<rootDir>/libs/ui/layout',
+    '<rootDir>/libs/ui/shared/elements',
     '<rootDir>/libs/ui/shared/icon',
     '<rootDir>/libs/ui/shared/material',
-    '<rootDir>/libs/ui/shared/tailwind',
-    '<rootDir>/libs/shared',
-    '<rootDir>/libs/api/core',
-    '<rootDir>/libs/ui/shared/elements'
+    '<rootDir>/libs/ui/shared/tailwind'
   ]
 };
