81ac918a51ad61d30f2eaa822c05b22ac47d6854
Usama Ansari <usama251993@gmail.com>
2021-08-14 13:04:53 +0530

    Migrated to Latest as of August 14, 2021

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
+    ...getJestProjects(),
     '<rootDir>/apps/api',
     '<rootDir>/apps/ui',
     '<rootDir>/libs/api/core',
