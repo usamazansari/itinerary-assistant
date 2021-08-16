7b3666613b1c1bae22d52f4076648cfc86ca7303
Usama Ansari <usama251993@gmail.com>
2021-08-16 19:34:30 +0530

    🚚 #109 Moved services from core to shared
    - ClipboardService
    - SnackbarService

--- libs/ui/core/src/lib/services/index.ts
+++ libs/ui/core/src/lib/services/index.ts
@@ -1,6 +1,6 @@
 export { CoreService } from './core.service';
 
-export { ClipboardService } from './clipboard/clipboard.service';
+// export { ClipboardService } from './clipboard/clipboard.service';
 export { EndpointService } from './endpoint/endpoint.service';
 export { RouterService } from './router/router.service';
-export { SnackbarService } from './snackbar/snackbar.service';
+// export { SnackbarService } from './snackbar/snackbar.service';
--- libs/ui/shared/elements/src/index.ts
+++ libs/ui/shared/elements/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/elements.module';
 
 export * from './lib/constants';
 export * from './lib/models';
+export * from './lib/services';
