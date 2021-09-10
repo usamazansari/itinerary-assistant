252c481245e2b50f56af223987cd2c0c8685b456
Usama Ansari <usama251993@gmail.com>
2021-09-10 16:06:45 +0530

    👽 Added wrapper methods in endpoint service
    - Added wrapper methods for `post`, `put` and `delete`
    - Updated ServerResponseMap

--- libs/ui/core/src/lib/services/endpoint/endpoint.service.ts
+++ libs/ui/core/src/lib/services/endpoint/endpoint.service.ts
@@ -27,15 +27,69 @@ export class EndpointService {
   constructor(private _http: HttpClient) { }
 
   /**
-   *
+   * Trigger a `get` request to the server
    *
    * @template ResponseType
-   * @param {string} url
-   * @return {*}  {Observable<ResponseType>}
+   * @param {string} url - server ednpoint
+   * @return {*}  {Observable<APIResponseModel<ResponseType>>}
+   *   Observable of response in a custom wrapper of type `APIResponseModel`
+   *
    * @memberof EndpointService
    */
   get<ResponseType>(url: string): Observable<APIResponseModel<ResponseType>> {
     return this._http.get<APIResponseModel<ResponseType>>(url)
+      .pipe(
+        delay(3000)
+      );
+  }
+
+  /**
+   * Trigger a `post` request to the server
+   *
+   * @template RequestBody Type of payload to be passed to the request
+   * @template ResponseType Type of expected response
+   * @param {string} url server endpoint
+   * @param {RequestBody} body request body
+   * @return {*}  {Observable<APIResponseModel<ResponseType>>}
+   *   Observable of response in a custom wrapper of type `APIResponseModel`
+   * @memberof EndpointService
+   */
+  post<RequestBody, ResponseType>(url: string, body: RequestBody): Observable<APIResponseModel<ResponseType>> {
+    return this._http.post<APIResponseModel<ResponseType>>(url, body)
+      .pipe(
+        delay(3000)
+      );
+  }
+
+  /**
+   * Trigger a `post` request to the server
+   *
+   * @template RequestBody Type of payload to be passed to the request
+   * @template ResponseType Type of expected response
+   * @param {string} url server endpoint
+   * @param {RequestBody} body request body
+   * @return {*}  {Observable<APIResponseModel<ResponseType>>}
+   *   Observable of response in a custom wrapper of type `APIResponseModel`
+   * @memberof EndpointService
+   */
+  put<RequestBody, ResponseType>(url: string, body: RequestBody): Observable<APIResponseModel<ResponseType>> {
+    return this._http.put<APIResponseModel<ResponseType>>(url, body)
+      .pipe(
+        delay(3000)
+      );
+  }
+
+  /**
+   * Trigger a `delete` request to the server
+   *
+   * @template ResponseType Type of expected response
+   * @param {string} url server endpoint
+   * @return {*}  {Observable<APIResponseModel<ResponseType>>}
+   *   Observable of response in a custom wrapper of type `APIResponseModel`
+   * @memberof EndpointService
+   */
+  delete<ResponseType>(url: string): Observable<APIResponseModel<ResponseType>> {
+    return this._http.delete<APIResponseModel<ResponseType>>(url)
       .pipe(
         delay(3000)
       );
