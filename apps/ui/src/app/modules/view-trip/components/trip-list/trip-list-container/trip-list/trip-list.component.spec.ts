88782b82dae4a9b784de25781cc700292d97d5ab
Usama Ansari <usama251993@gmail.com>
2021-09-14 21:51:37 +0530

    🧪 Added Tests for TripList Skeleton

--- apps/ui/src/app/modules/view-trip/components/trip-list/trip-list-container/trip-list/trip-list.component.spec.ts
+++ apps/ui/src/app/modules/view-trip/components/trip-list/trip-list-container/trip-list/trip-list.component.spec.ts
@@ -1,25 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { TripListComponent } from './trip-list.component';
 
 describe('TripListComponent', () => {
   let component: TripListComponent;
+  let debugEl: DebugElement;
   let fixture: ComponentFixture<TripListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TripListComponent ]
+      declarations: [TripListComponent],
+      providers: [
+        { provide: ComponentFixtureAutoDetect, useValue: true }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TripListComponent);
-    component = fixture.componentInstance;
+    debugEl = fixture.debugElement;
+    component = debugEl.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('TripListComponent should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('TripListComponent should display when API Call is triggered', () => {
+    const divDebugEl: DebugElement = debugEl.query(By.css('#skeleton-container-div'));
+    const div: HTMLDivElement = divDebugEl.nativeElement;
+    component.flags.shell.progress = true;
+    // fixture.detectChanges();
+    expect(div).toBeDefined();
   });
 });
