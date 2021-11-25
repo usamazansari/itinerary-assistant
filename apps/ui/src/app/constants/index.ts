450d691eccaf496cd5b401850879f479ac2df82b
Usama Ansari <usama251993@gmail.com>
2021-11-25 13:07:03 +0530

    📝 Updated documentation in index.ts of the ui app

---
+++ apps/ui/src/app/constants/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Only those constants which satisfy the follow conditions are placed in this directory:
+ *
+ * 1. The constant must be used only in this module
+ * 2. The constant must be used in more than one component in this module
+ */
---
+++ apps/ui/src/app/models/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Only those interfaces which satisfy the follow conditions are placed in this directory:
+ *
+ * 1. The interface must be used only in this module
+ * 2. The interface must be used in more than one component in this module
+ */
