92c64bc0b9797401a0d7a3c125a9c7d6e7af6f47
Usama Ansari <usama251993@gmail.com>
2021-11-26 12:17:40 +0530

    🗑️ cleanup after components migration

--- libs/ui/shared/elements/src/lib/models/index.ts
+++ libs/ui/shared/elements/src/lib/models/index.ts
@@ -1,4 +1,5 @@
 export type { IconModel } from './icon/icon.model';
 export type { ImageModel } from './image/image.model';
-export type { LoaderConfigModel } from '../components/loader';
 export type { FlagModel } from './flag/flag.model';
+
+export type { LoaderConfigModel } from '../components/loader';
