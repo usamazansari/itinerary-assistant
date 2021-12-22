7db7313bfc79a49c0cfcbb4966409a1b36b533e1
Usama Ansari <usama251993@gmail.com>
2021-12-22 20:57:10 +0530

    ✨ feat - just to get by error handler in api-trip

--- libs/api/features/trip/src/lib/controllers/trip/trip.controller.ts
+++ libs/api/features/trip/src/lib/controllers/trip/trip.controller.ts
@@ -1,21 +1,39 @@
 import { Controller, Get, Param } from '@nestjs/common';
 
-import type { APIResponseModel } from '../../imports/models';
+import type { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { APIResponse } from '../../imports/models';
+import { HttpStatus } from '../../imports/constants';
 import type { TripOverview } from '../../imports/entities';
 
 import { TripService } from '../../services';
 
 @Controller('trip')
 export class TripController {
+
   constructor(private _service: TripService) { }
 
   @Get('view-trip')
-  fetchTripList(): Promise<APIResponseModel<TripOverview[]>> {
-    return this._service.fetchTripList();
+  fetchTripList(): Observable<APIResponse<TripOverview[]>> {
+    return this._service.fetchTripList()
+      .pipe(
+        map(
+          response => (response instanceof Error)
+            ? ({ error: response })
+            : ({ data: response })
+        ),
+        map(
+          response => {
+            if (!!response.error)
+              return new APIResponse({ data: null, error: response.error, status: HttpStatus.InternalServerError });
+            return new APIResponse({ data: response.data, error: null, status: HttpStatus.InternalServerError });
+          })
+      );
   }
 
   @Get('view-trip/:tripName')
   fetchTripOverview(@Param('tripName') tripName: string) {
-    return this._service.fetchTripOverview(tripName);
+    return this._service.fetchTrip(tripName);
   }
 }
--- libs/api/features/trip/src/lib/services/trip/trip.service.ts
+++ libs/api/features/trip/src/lib/services/trip/trip.service.ts
@@ -1,23 +1,14 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 
-import { HttpStatus } from '../../imports/constants';
-import { TripOverview } from '../../imports/entities';
-import { APIResponseModel } from '../../imports/models';
-import type { TripOverviewModel } from '../../imports/models';
+import type { Observable } from 'rxjs';
+import { from } from 'rxjs';
 
-import { goa, ladakh, manali, northEast } from '../../mock';
+import { TripOverview } from '../../imports/entities';
 import { TRIP_REPOSITORY } from '../../constants';
 
 @Injectable()
 export class TripService {
-  #tripOverviewDict: { [tripName: string]: TripOverviewModel; } = {
-    goa,
-    ladakh,
-    manali,
-    northEast
-  };
-  #tripOverview!: TripOverviewModel;
 
   constructor(
     @Inject(TRIP_REPOSITORY)
@@ -25,25 +16,20 @@ export class TripService {
   ) { }
 
 
-  fetchTripList(): Promise<APIResponseModel<TripOverview[]>> {
-    return this._repository.find()
-      .then(trips => new APIResponseModel(
-        trips,
-        null,
-        HttpStatus.Ok
-      ))
-      .catch(error => new APIResponseModel(
-        [],
-        error,
-        error.status ?? HttpStatus.InternalServerError
-      ));
+  fetchTripList(): Observable<TripOverview[]> {
+    return from(
+      this._repository.find()
+        .then(
+          data => data,
+          error => { throw error; }
+        )
+        .catch(
+          error => error
+        )
+    );
   };
 
   fetchTrip(id: string | number) {
     return this._repository.findOne(id);
   }
-
-  // fetchTripList(): TripListItemModel[] {
-  //   return [...this.#tripList];
-  // }
 }
