5ced910898f60d8164c06c42aec5369674e48566
Usama Ansari <usama251993@gmail.com>
2021-12-23 20:02:50 +0530

    📝 docs - added documentation for generic object

--- libs/shared/data/src/lib/models/utils/index.ts
+++ libs/shared/data/src/lib/models/utils/index.ts
@@ -1,8 +1,13 @@
-// TODO: Usama Ansari - 🧐 Documentation required.
+/**
+ * ### Generic Object
+ *
+ * Any JavaScript Object can be used as a generic object. This type is
+ * used in replacement of `unknown` or `any` when there is surety that
+ * the entity would definitely be an object.
+ *
+ * @export
+ * @interface GenericObject
+ */
 export interface GenericObject {
-  [key: string]:
-  string | string[]
-  | number | number[]
-  | boolean | boolean[]
-  | GenericObject | GenericObject[];
+  [key: string]: string | string[] | number | number[] | boolean | boolean[] | GenericObject | GenericObject[];
 }
