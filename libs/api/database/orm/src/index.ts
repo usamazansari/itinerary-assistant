6b1736f347b7728dd7281b3ae7f2b9507e5f2ac4
Usama Ansari <usamazansari@outlook.com>
2022-02-16 12:25:27 +0530

    🚚 renamed to orm-module

--- libs/api/database/orm/src/index.ts
+++ libs/api/database/orm/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/api-database-orm.module';
+export * from './lib/orm.module';
--- libs/api/database/orm/src/lib/orm.module.ts
+++ libs/api/database/orm/src/lib/orm.module.ts
@@ -5,4 +5,4 @@ import { Module } from '@nestjs/common';
   providers: [],
   exports: []
 })
-export class ApiDatabaseOrmModule {}
+export class OrmModule {}
