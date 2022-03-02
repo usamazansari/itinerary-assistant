7697b03ff04e0dcaf28c7281327a121cc8678725
Usama Ansari <usamazansari@outlook.com>
2022-03-02 13:26:44 +0530

    🚚 moved core to api/core

--- libs/api/core/jest.config.js
+++ libs/api/core/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  displayName: 'core',
-  preset: '../../jest.preset.js',
+  displayName: 'api-core',
+  preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json'
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/core'
+  coverageDirectory: '../../../coverage/libs/api/core'
 };
