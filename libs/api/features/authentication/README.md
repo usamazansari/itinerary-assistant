543ecd644ecd13cba0ac121c2940f139f88ad99c
Usama Ansari <usamazansari@outlook.com>
2022-03-07 21:01:16 +0530

    📦 added authentication library

---
+++ libs/api/features/authentication/README.md
@@ -0,0 +1,7 @@
+# api-features-authentication
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test api-features-authentication` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/api/features/authentication/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  displayName: 'api-features-authentication',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json'
+    }
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory:
+    '../../../../coverage/libs/api/features/authentication'
+};
