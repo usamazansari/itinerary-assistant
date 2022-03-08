f666ecfc8b78cfe5ee13609d0a10c49ede98dcbf
Usama Ansari <usamazansari@outlook.com>
2022-03-08 12:44:42 +0530

    🏹 social connection dto

--- libs/shared/data/src/lib/models/database/social-connection.model.ts
+++ libs/shared/data/src/lib/models/database/social-connection.model.ts
@@ -1,22 +1,52 @@
 export interface ISocialConnection {
+  id: string;
   name: string;
   url: string;
+  userId: string;
 }
 
 const SocialConnectionStub: ISocialConnection = {
+  id: `new-connection-${new Date().toISOString()}`,
   name: '',
-  url: ''
+  url: '',
+  userId: `new-connection-for-user-${new Date().toISOString()}`
 };
 
 export class SocialConnection implements ISocialConnection {
+  id: string;
   name: string;
   url: string;
+  userId: string;
+
+  constructor({
+    id = SocialConnectionStub.id,
+    name = SocialConnectionStub.name,
+    url = SocialConnectionStub.url,
+    userId = SocialConnectionStub.userId
+  }: Partial<ISocialConnection>) {
+    this.id = id;
+    this.name = name;
+    this.url = url;
+    this.userId = userId;
+  }
+}
+
+type SocialConnectionDTOOmitType = 'id';
+
+export class SocialConnectionDTO
+  implements Omit<ISocialConnection, SocialConnectionDTOOmitType>
+{
+  name: string;
+  url: string;
+  userId: string;
 
   constructor({
     name = SocialConnectionStub.name,
-    url = SocialConnectionStub.url
+    url = SocialConnectionStub.url,
+    userId = SocialConnectionStub.userId
   }: Partial<ISocialConnection>) {
     this.name = name;
     this.url = url;
+    this.userId = userId;
   }
 }
