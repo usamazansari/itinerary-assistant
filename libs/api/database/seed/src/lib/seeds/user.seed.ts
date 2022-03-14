8b7908d897b4a3446210b8f9c1da73537e4ad528
Usama Ansari <usamazansari@outlook.com>
2022-03-14 14:38:09 +0530

    🌱 updated user seed

---
+++ libs/api/database/seed/src/lib/seeds/user.seed.ts
@@ -0,0 +1,21 @@
+import { Gender, User } from '../imports/models';
+
+export const UserSeed: User[] = [
+  {
+    id: 'user-1',
+    username: 'user-1-name',
+    dateOfBirth: new Date(1993, 9, 25),
+    address: 'user-1-address',
+    gender: Gender.Male,
+    email: 'usama251993@gmail.com',
+    phone: '+91 80977 28350',
+    website: 'https://usamaansari.com',
+    socialConnection: [
+      'user-1-social-1',
+      'user-1-social-2',
+      'user-1-social-3'
+    ],
+    photo: 'user-1-photo',
+    identifications: ['user-1-id-1', 'user-1-id-2']
+  }
+];
