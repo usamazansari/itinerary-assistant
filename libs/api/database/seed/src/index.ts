d7fc700a87eaa8e329c8230666f985c544ea734c
Usama Ansari <usamazansari@outlook.com>
2022-03-14 14:04:36 +0530

    🗃️ added placeholder files for seeds

--- libs/api/database/seed/src/index.ts
+++ libs/api/database/seed/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/seed.module';
+export * from './lib/seeds';
---
+++ libs/api/database/seed/src/lib/seeds/index.ts
@@ -0,0 +1,13 @@
+export * from './tenure.seed';
+
+export * from './coordinates.seed';
+export * from './timezone.seed';
+export * from './location.seed';
+export * from './address.seed';
+
+export * from './photo.seed';
+export * from './identification.seed';
+export * from './social-connection.seed';
+
+export * from './user-name.seed';
+export * from './user.seed';
