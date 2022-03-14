c2a3056c08f2d3773a908bbe0d95fc680cb6795d
Usama Ansari <usamazansari@outlook.com>
2022-03-14 14:23:06 +0530

    🌱 added photo seed

---
+++ libs/api/database/seed/src/lib/seeds/photo.seed.ts
@@ -0,0 +1,11 @@
+import { Photo } from '../imports/models';
+
+export const PhotoSeed: Photo[] = [
+  {
+    id: 'user-1-photo',
+    large: 'https://randomuser.me/api/portraits/usamaansari',
+    medium: 'https://randomuser.me/api/portraits/medium/usamaansari',
+    thumbnail: 'https://randomuser.me/api/portraits/thumb/usamaansari',
+    user: 'user-1'
+  }
+];
