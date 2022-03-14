ef3ebdf19c1df5d671a5921eb4441b60f82a24f3
Usama Ansari <usamazansari@outlook.com>
2022-03-14 14:06:02 +0530

    🌱 added coordinates seed

---
+++ libs/api/database/seed/src/lib/seeds/coordinates.seed.ts
@@ -0,0 +1,10 @@
+import { Coordinates } from '../imports/models';
+
+export const CoordinatesSeed: Coordinates[] = [
+  {
+    id: 'user-1-address-location-coordinates',
+    latitude: 19.0628191,
+    longitude: 72.8808394,
+    location: 'user-1-address-location'
+  }
+];
