2365ea0e124cf41aa3cd01eadf26a4a0a4b3183d
Usama Ansari <usamazansari@outlook.com>
2022-03-14 14:22:08 +0530

    🌱 added social connection seed

---
+++ libs/api/database/seed/src/lib/seeds/social-connection.seed.ts
@@ -0,0 +1,22 @@
+import { SocialConnection } from '../imports/models';
+
+export const SocialConnectionSeed: SocialConnection[] = [
+  {
+    id: 'user-1-social-1',
+    url: 'https://www.linkedin.com/in/usama-ansari-81380080/',
+    name: 'LinkedIn',
+    user: 'user-1'
+  },
+  {
+    id: 'user-1-social-2',
+    url: 'https://instagram.com/usamazansari',
+    name: 'Instagram',
+    user: 'user-1'
+  },
+  {
+    id: 'user-1-social-3',
+    url: 'https://github.com/usamazansari',
+    name: 'GitHub',
+    user: 'user-1'
+  }
+];
