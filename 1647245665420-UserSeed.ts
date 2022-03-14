c926a4af63f0c9721c530bee7f60b6bd239ff863
Usama Ansari <usamazansari@outlook.com>
2022-03-14 13:50:47 +0530

    ✈️ added seed migration file

---
+++ 1647245665420-UserSeed.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class UserSeed1647245665420 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
