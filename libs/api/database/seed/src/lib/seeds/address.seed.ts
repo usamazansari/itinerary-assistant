89e8e07952ee708489140d38148a750b6865b675
Usama Ansari <usamazansari@outlook.com>
2022-03-14 14:29:54 +0530

    🌱 added address seed

---
+++ libs/api/database/seed/src/lib/seeds/address.seed.ts
@@ -0,0 +1,20 @@
+import { Address } from '../imports/models';
+
+export const AddressSeed: Address[] = [
+  {
+    id: 'user-1-address',
+    room: '1301',
+    wing: 'A',
+    apartment: 'Sayba Nx',
+    street: 'S. G. Barve Marg',
+    landmark: 'Opposite Kurla Railway Station',
+    locality: 'Nehru Nagar',
+    suburb: 'Kurla East',
+    city: 'Mumbai',
+    state: 'Maharashtra',
+    country: 'India',
+    zip: '400024',
+    name: 'Rent',
+    location: 'user-1-address-location'
+  }
+];
