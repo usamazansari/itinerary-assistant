b86360885b95764628c68c7b3a6244d599019231
Usama Ansari <usamazansari@outlook.com>
2022-03-14 14:15:26 +0530

    🌱 added location seed

---
+++ libs/api/database/seed/src/lib/seeds/location.seed.ts
@@ -0,0 +1,10 @@
+import { Location } from '../imports/models';
+
+export const LocationSeed: Location[] = [
+  {
+    id: 'user-1-address-location',
+    plusCode: '3V7J+59 Mumbai, Maharashtra',
+    coordinates: 'user-1-address-location-coordinates',
+    timezone: 'user-1-address-location-timezone'
+  }
+];
