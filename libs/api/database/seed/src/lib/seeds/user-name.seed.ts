dd069c514c22c7ef6f7ff5b8b455376caa3b6756
Usama Ansari <usamazansari@outlook.com>
2022-03-14 14:04:58 +0530

    🌱 added user-name seed

---
+++ libs/api/database/seed/src/lib/seeds/user-name.seed.ts
@@ -0,0 +1,10 @@
+import { UserName } from '../imports/models';
+
+export const UserNameSeed: UserName[] = [
+  {
+    id: 'user-1-name',
+    first: 'Usama',
+    last: 'Ansari',
+    full: 'Usama Ansari'
+  }
+];
