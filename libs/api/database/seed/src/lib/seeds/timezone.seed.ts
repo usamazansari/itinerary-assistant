b9a62caef17bd518af7b0dc99063943267703b1e
Usama Ansari <usamazansari@outlook.com>
2022-03-14 14:06:48 +0530

    🌱 added timezone seed

---
+++ libs/api/database/seed/src/lib/seeds/timezone.seed.ts
@@ -0,0 +1,10 @@
+import { Timezone } from '../imports/models';
+
+export const TimezoneSeed: Timezone[] = [
+  {
+    id: 'user-1-address-location-timezone',
+    offset: 5.5,
+    description: 'Asia/Kolkata',
+    location: 'user-1-address-location'
+  }
+];
