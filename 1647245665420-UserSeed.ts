17170827abc306d17496ffffc0303698e4dc5df8
Usama Ansari <usamazansari@outlook.com>
2022-03-19 12:58:37 +0530

    🔥 removed failed migration of UserSeed

--- 1647245665420-UserSeed.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class UserSeed1647245665420 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
