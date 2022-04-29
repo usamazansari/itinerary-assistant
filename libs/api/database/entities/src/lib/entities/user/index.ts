ace3afc6fab9da282d88fda80e443fee2f4baa13
Usama Ansari <usamazansari@outlook.com>
2022-04-29 20:42:43 +0530

    📝 renamed user to person in entity

--- libs/api/database/entities/src/lib/entities/user/index.ts
+++ libs/api/database/entities/src/lib/entities/user/index.ts
@@ -3,4 +3,4 @@ export * from './demographics/demographics.entity';
 export * from './identification/identification.entity';
 export * from './photo/photo.entity';
 export * from './social-connection/social-connection.entity';
-export * from './user.entity';
+export * from './person.entity';
---
+++ libs/api/database/entities/src/lib/entities/user/person.entity.ts
@@ -0,0 +1,57 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+import { Gender } from '../imports/constants';
+import {
+  IPerson,
+  Address as AddressModel,
+  Demographics as DemographicsModel,
+  Identification as IdentificationModel,
+  Photo as PhotoModel,
+  SocialConnection as SocialConnectionModel
+} from '../imports/models';
+import {
+  Address as AddressEntity,
+  Identification as IdentificationEntity,
+  Demographics as DemographicsEntity,
+  Photo as PhotoEntity,
+  SocialConnection as SocialConnectionEntity
+} from '.';
+
+@ObjectType()
+export class Person implements IPerson {
+  @Field(() => ID)
+  id!: string;
+
+  @Field()
+  fullName!: string;
+
+  @Field(() => DemographicsEntity)
+  demographics!: DemographicsModel;
+
+  @Field()
+  email!: string;
+
+  @Field()
+  phone!: string;
+
+  @Field()
+  dateOfBirth!: Date;
+
+  @Field(() => Gender)
+  gender!: Gender;
+
+  @Field()
+  website!: string;
+
+  @Field(() => AddressEntity)
+  address!: AddressModel;
+
+  @Field(() => [IdentificationEntity])
+  identifications!: IdentificationModel[];
+
+  @Field(() => [SocialConnectionEntity])
+  socialConnections!: SocialConnectionModel[];
+
+  @Field(() => [PhotoEntity])
+  photos!: PhotoModel[];
+}
