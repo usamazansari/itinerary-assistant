cbb77a0b6f28c111cef9a1801e4c8b46dbf0256e
Usama Ansari <usamazansari@outlook.com>
2022-06-12 13:46:25 +0530

    ⏰ added time parsers

--- libs/api/database/neo4j/src/lib/utils/neo4j-utility/neo4j.utility.ts
+++ libs/api/database/neo4j/src/lib/utils/neo4j-utility/neo4j.utility.ts
@@ -1,8 +1,8 @@
-import { DateTime } from 'neo4j-driver';
+import { DateTime, types } from 'neo4j-driver';
 
 import { Neo4jNode, Neo4jRelationship } from '../../models';
 
-export const parseDateTime = (dateTime: DateTime): Date => {
+export const parseFromDateTime = (dateTime: DateTime): Date => {
   const { year, month, day, hour, minute, second, nanosecond } = dateTime;
 
   return new Date(
@@ -13,6 +13,21 @@ export const parseDateTime = (dateTime: DateTime): Date => {
     +minute || 0,
     +second || 0,
     (+nanosecond || 0) / 1000000
+  );
+};
+
+export const parseToDateTime = (date: Date): DateTime<number> => {
+  const { DateTime: DateTimeConstructor } = types;
+
+  return new DateTimeConstructor<number>(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds() * 1000000,
+    date.getTimezoneOffset() * 60
   );
 };
 
