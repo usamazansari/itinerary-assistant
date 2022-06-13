0394940d5044fddc83866fef9127a33120f01418
Usama Ansari <usamazansari@outlook.com>
2022-06-13 09:47:32 +0530

    ⚡ get set person

--- libs/domain/src/lib/models/entities/person/person.model.ts
+++ libs/domain/src/lib/models/entities/person/person.model.ts
@@ -68,9 +68,65 @@ export class Person implements IPerson {
     if (!!gender) this.gender = gender;
   }
 
-  generateUUID(): string {
-    this.id = uuid();
+  getId(): string {
     return this.id;
+  }
+
+  setId(id = ''): void {
+    this.id = id;
+  }
+
+  getFullName(): string {
+    return this.fullName;
+  }
+
+  setFullName(fullName: string): void {
+    this.fullName = fullName;
+  }
+
+  getEmail(): string {
+    return this.email;
+  }
+
+  setEmail(email: string): void {
+    this.email = email;
+  }
+
+  getPhone(): string {
+    return this.phone;
+  }
+
+  setPhone(phone: string): void {
+    this.phone = phone;
+  }
+
+  getWebsite(): string {
+    return this.website;
+  }
+
+  setWebsite(website: string): void {
+    this.website = website;
+  }
+
+  getDateOfBirth(): Date | undefined {
+    return this.dateOfBirth;
+  }
+
+  setDateOfBirth(dateOfBirth: Date): void {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  getGender(): Gender | undefined {
+    return this.gender;
+  }
+
+  setGender(gender: Gender): void {
+    this.gender = gender;
+  }
+
+  generateUUID(): string {
+    this.setId(uuid());
+    return this.getId();
   }
 }
 
