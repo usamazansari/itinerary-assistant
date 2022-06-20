a10e9cb2f7d05382263d1b89543d109d9f6cf2e6
Usama Ansari <usamazansari@outlook.com>
2022-06-20 20:18:28 +0530

    🔥 removed unused core controller

--- libs/api/core/src/lib/controllers/core/core.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('api-core')
-export class CoreController {}
--- libs/api/core/src/lib/core.module.ts
+++ libs/api/core/src/lib/core.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { GraphQLModule, Neo4jModule } from './imports/modules';
 
-import { CoreController } from './controllers';
 import { CoreService } from './services';
 
 @Module({
-  controllers: [CoreController],
   imports: [GraphQLModule, Neo4jModule.forRootAsync()],
   providers: [CoreService],
   exports: [GraphQLModule, Neo4jModule]
