69e7e0a16c7b2727035e92c819c372d0c4ea6812
Usama Ansari <usamazansari@outlook.com>
2022-07-20 12:51:04 +0530

    🌱 added trip seeds

---
+++ libs/api/database/seed/src/lib/seeds/trip/index.ts
@@ -0,0 +1,4 @@
+export * from './overview';
+export * from './list';
+
+export * from './goa.trip';
---
+++ libs/api/database/seed/src/lib/seeds/trip/overview/index.ts
@@ -0,0 +1,4 @@
+export * from './goa.overview';
+export * from './ladakh.overview';
+export * from './manali.overview';
+export * from './north-east.overview';
