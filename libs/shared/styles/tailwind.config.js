d4e5777e06b0014d104a80d5fbc2460079158d40
Usama Ansari <usamazansari@outlook.com>
2022-07-21 09:51:20 +0530

    ❌ disconnect shared-styles

--- libs/shared/styles/tailwind.config.js
+++ libs/shared/styles/tailwind.config.js
@@ -3,8 +3,8 @@ const Theme = require('./src');
 module.exports = {
   theme: {
     extend: {
-      colors: { ...Theme.material.colors },
-      boxShadow: { ...Theme.material.elevations }
+      // colors: { ...Theme.material.colors },
+      // boxShadow: { ...Theme.material.elevations }
     }
   }
 };
