2b63aabefc81281710403d8854be388f0aed4924
Usama Ansari <usamazansari@outlook.com>
2022-07-21 09:56:34 +0530

    🗑️ removed ui-design-components-elementary

--- libs/ui/design/components/elementary/src/lib/components/loader/component/loader-container/loader/loader.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({
-  selector: 'ia-components-loader',
-  templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class LoaderComponent {}
