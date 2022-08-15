400a93cb7ab10f7a9934921226f08489038cb948
Usama Ansari <usamazansari@outlook.com>
2022-08-15 11:15:12 +0530

    ❌ toggles OFF resolvefield in seed data

--- apps/api/src/app/app.resolver.ts
+++ apps/api/src/app/app.resolver.ts
@@ -1,26 +1,15 @@
-import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
 
-import { Address as AddressEntity, Person as Entity } from './imports/entities';
-import { Address, Person } from './imports/models';
-import {
-  EramAddresses,
-  EramPerson,
-  UsamaAddresses,
-  UsamaPerson
-} from './imports/seed';
+import { Person as Entity } from './imports/entities';
+import { Person } from './imports/models';
+import { EramPerson, UsamaPerson } from './imports/seed';
 
 const people: Person[] = [UsamaPerson, EramPerson];
-const addresses: Address[] = [...UsamaAddresses, ...EramAddresses];
 
 @Resolver(() => Entity)
 export class AppResolver {
   @Query(() => Entity)
   gqlSanity() {
     return Math.random() > 0.5 ? people[0] : people[1];
-  }
-
-  @ResolveField(() => [AddressEntity], { name: 'addresses' })
-  getAddress(@Parent() { id }: Entity) {
-    return addresses.filter(address => new RegExp(id).test(address.id));
   }
 }
