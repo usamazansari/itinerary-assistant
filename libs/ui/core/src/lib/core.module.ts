4224743f38b87e1df13ee463636778eea2298ebe
Usama Ansari <usamazansari@outlook.com>
2022-08-16 12:41:14 +0530

    🧅 added ui/core library

---
+++ libs/ui/core/src/lib/core.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+
+@NgModule({
+  imports: [CommonModule]
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (!!parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only'
+      );
+    }
+  }
+}
