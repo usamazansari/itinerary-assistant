c46c3dbe1539604dbe9be0e4bdc492c87865af05
Usama Ansari <usamazansari@outlook.com>
2022-08-20 22:43:49 +0530

    ⚡ updated providers in trip module

--- libs/api/features/trip/src/lib/trip.module.ts
+++ libs/api/features/trip/src/lib/trip.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 
+import { TripHelper } from './helpers';
+import { TripRepository } from './repositories';
+import { TripResolver } from './resolvers';
+import { CoreService, TripService } from './services';
+
 @Module({
-  controllers: [],
-  providers: [],
-  exports: []
+  providers: [
+    CoreService,
+    TripHelper,
+    TripRepository,
+    TripResolver,
+    TripService
+  ]
 })
 export class TripModule {}
