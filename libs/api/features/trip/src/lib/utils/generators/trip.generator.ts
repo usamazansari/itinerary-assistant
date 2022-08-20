fb20c27201f6ac8142f66bf4c4ae4fecd29e2c99
Usama Ansari <usamazansari@outlook.com>
2022-08-20 20:29:09 +0530

    🩹 updated trip generator

--- libs/api/features/trip/src/lib/utils/generators/trip.generator.ts
+++ libs/api/features/trip/src/lib/utils/generators/trip.generator.ts
@@ -1,3 +1,7 @@
+import { DateTime } from 'neo4j-driver';
+
+import { toNeo4jDateTime, toJSDate } from '../../imports/utils';
+
 import { Trip, TripDTO } from '../../models';
 
 const generateCreateTripObject = ({
@@ -13,8 +17,29 @@ const generateCreateTripObject = ({
   if (!!id) _ = { ..._, id };
   if (!!source) _ = { ..._, source };
   if (!!destination) _ = { ..._, destination };
-  if (!!start) _ = { ..._, start };
-  if (!!end) _ = { ..._, end };
+  if (!!start) _ = { ..._, start: toNeo4jDateTime(start) };
+  if (!!end) _ = { ..._, end: toNeo4jDateTime(end) };
+  if (!!name) _ = { ..._, name };
+  if (!!accomplices) _ = { ..._, accomplices };
+
+  return { ..._ } as Trip;
+};
+
+const generateReadTripObject = ({
+  id,
+  source,
+  destination,
+  start,
+  end,
+  name,
+  accomplices
+}: Partial<Trip>) => {
+  let _ = {};
+  if (!!id) _ = { ..._, id };
+  if (!!source) _ = { ..._, source };
+  if (!!destination) _ = { ..._, destination };
+  if (!!start) _ = { ..._, start: toJSDate((<unknown>start) as DateTime) };
+  if (!!end) _ = { ..._, end: toJSDate((<unknown>end) as DateTime) };
   if (!!name) _ = { ..._, name };
   if (!!accomplices) _ = { ..._, accomplices };
 
@@ -31,14 +56,12 @@ const generateUpdateTripObject = ({
   let _ = {};
   if (!!source) _ = { ..._, ['trip.source']: source };
   if (!!destination) _ = { ..._, ['trip.destination']: destination };
-  if (!!start) _ = { ..._, ['trip.start']: start };
-  if (!!end) _ = { ..._, ['trip.end']: end };
+  if (!!start) _ = { ..._, ['trip.start']: toNeo4jDateTime(start) };
+  if (!!end) _ = { ..._, ['trip.end']: toNeo4jDateTime(end) };
   if (!!name) _ = { ..._, ['trip.name']: name };
 
   return _ as TripDTO;
 };
-
-const generateReadTripObject = generateCreateTripObject;
 
 export {
   generateCreateTripObject,
