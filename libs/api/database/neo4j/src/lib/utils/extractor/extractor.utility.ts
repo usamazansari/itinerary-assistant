cd3e7aeec241e989011216c23b9e23aeb8e319d9
Usama Ansari <usamazansari@outlook.com>
2022-08-31 19:01:08 +0530

    refactor(api): ⚡ updated extractor utility implementation

--- libs/api/database/neo4j/src/lib/utils/extractor/extractor.utility.ts
+++ libs/api/database/neo4j/src/lib/utils/extractor/extractor.utility.ts
@@ -1,11 +1,9 @@
-import { Neo4jOutput } from '../../models';
-
 import { BaseModel } from '../../imports/models';
 
-// TODO: Usama Ansari - Documentation Required!
+import { Neo4jOutput } from '../../models';
 
 const extractEntity = <T = unknown>(r: Neo4jOutput<T>) =>
-  r.map(({ output: o }) => o);
+  r.map(({ output: { properties: p } }) => p);
 
 const nodeMapper = <T extends BaseModel>(
   c: { new (i: Partial<T>): T },
