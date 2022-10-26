77a643157360c657a7ba824ccc689a376c4d9d7e
Usama Ansari <usamazansari@outlook.com>
2022-10-26 22:50:18 +0200

    feat(frontend): 🎉 initiated angular app

---
+++ apps/frontend/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'itinerary-assistant-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'frontend-client';
+}
