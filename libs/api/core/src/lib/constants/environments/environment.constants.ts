42156621c0bf14e0c2012a811d7bcd0a3c74c1e3
Usama Ansari <usamazansari@outlook.com>
2022-10-30 11:23:44 +0100

    feat(api): 🎉 implemented core module

---
+++ libs/api/core/src/lib/constants/environments/environment.constants.ts
@@ -0,0 +1,16 @@
+const ENVIRONMENT_VARIABLES = {
+  KEYS: {
+    NODE_ENV: 'NODE_ENV',
+    NEO4J: {
+      URI: 'NEO4J_URI',
+      USERNAME: 'NEO4J_USERNAME',
+      PASSWORD: 'NEO4J_PASSWORD'
+    }
+  },
+  VALUES: {
+    DEVELOPMENT: 'development',
+    PRODUCTION: 'production'
+  }
+};
+
+export { ENVIRONMENT_VARIABLES };
---
+++ libs/api/core/src/lib/imports/libraries/index.ts
@@ -0,0 +1,2 @@
+export { GraphQLModule } from '@itinerary-assistant/api/database/graphql';
+export { Neo4jModule } from '@itinerary-assistant/api/database/neo4j';
