b2c840fa3b123a477ac234198130798a631ce2a0
Usama Ansari <usamazansari@outlook.com>
2022-10-30 15:50:47 +0100

    fix(api): ⚡updated person api

--- libs/api/features/person/src/lib/services/address/address.service.ts
+++ libs/api/features/person/src/lib/services/address/address.service.ts
@@ -11,9 +11,23 @@ export class AddressService {
     private _repository: AddressRepository
   ) {}
 
+  async getAddresses() {
+    const result = await this._repository.getAddresses();
+    const response = this._helper.extractAddresses(result);
+
+    if (!response?.length)
+      throw new Error('No Addresses found in the database.');
+    return response;
+  }
+
   async getAddress(id = '') {
     const result = await this._repository.getAddress(id);
     const [response] = this._helper.extractAddresses(result);
+
+    if (!response)
+      throw new Error(
+        `Cannot fetch the Address having id: '${id}'. The address might not exist in the database.`
+      );
     return response;
   }
 
@@ -21,48 +35,92 @@ export class AddressService {
     const address = this._helper.createAddressPayload(dto);
     const result = await this._repository.createAddress(address);
     const [response] = this._helper.extractAddresses(result);
+
+    if (!response) throw new Error(`Cannot create the new Address`);
     return response;
   }
 
-  async updateAddress(id: string, dto: AddressDTO) {
+  async updateAddress(id = '', dto: AddressDTO) {
     const update = this._helper.updateAddressPayload(dto);
     const result = await this._repository.updateAddress(id, update);
     const [response] = this._helper.extractAddresses(result);
+
+    if (!response)
+      throw new Error(
+        `Cannot update the Address having id: '${id}'. The address might not exist in the database.`
+      );
     return response;
   }
 
-  async deleteAddress(id: string) {
+  async deleteAddress(id = '') {
     const result = await this._repository.deleteAddress(id);
     const [response] = this._helper.extractAddresses(result);
+
+    if (!response)
+      throw new Error(
+        `Cannot delete the Address having id: '${id}'. The address might not exist in the database.`
+      );
     return response;
   }
 
-  async getResidents(id = '') {
-    const result = await this._repository.getResidents(id);
-    return this._helper.extractPeople(result);
+  async getResidents(addressId = '') {
+    const result = await this._repository.getResidents(addressId);
+    const response = this._helper.extractPeople(result);
+
+    return response;
   }
 
   async associateAddressWithPerson({
-    addressId,
-    personId
+    addressId = '',
+    personId = ''
   }: AddressPersonAssociation) {
     const check = await this.checkAddressOfRelationship({
       addressId,
       personId
     });
-    const result = check
-      ? await this._repository.getAddress(addressId)
-      : await this._repository.associateAddressWithPerson({
-          addressId,
-          personId
-        });
+
+    if (check) return this.getAddress(addressId);
+    const result = await this._repository.associateAddressWithPerson({
+      addressId,
+      personId
+    });
     const [response] = this._helper.extractAddresses(result);
+
+    if (!response)
+      throw new Error(
+        `Unable to associate the address having id: ${addressId} with person having id: ${personId}.`
+      );
     return response;
   }
 
+  async disassociateAddressWithPerson({
+    addressId = '',
+    personId = ''
+  }: AddressPersonAssociation) {
+    const check = await this.checkAddressOfRelationship({
+      addressId,
+      personId
+    });
+
+    if (!check) return this.getAddress();
+    else {
+      const result = await this._repository.disassociateAddressWithPerson({
+        addressId,
+        personId
+      });
+      const [response] = this._helper.extractAddresses(result);
+
+      if (!response)
+        throw new Error(
+          `Unable to disassociate the address having id: ${addressId} with person having id: ${personId}.`
+        );
+      return response;
+    }
+  }
+
   async checkAddressOfRelationship({
-    addressId,
-    personId
+    addressId = '',
+    personId = ''
   }: AddressPersonAssociation) {
     const result = await this._repository.checkAddressOfRelationship({
       addressId,
